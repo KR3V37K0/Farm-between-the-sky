201a380763d930cde26bf881dba9d1dc525a2a68
Ruruner <rustananisev448@gmail.com>
2024-07-24 15:07:41 +0500

    sun move

--- Assets/Sc/DayChangerSc.cs
+++ Assets/Sc/DayChangerSc.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class DayChangerSc : MonoBehaviour
 {
-    [Range (0,1)] 
-    public float BeginningOfNight, TimeOfDay;
-    public float LengthOfDay;
+    [Range(0, 1)]
+    public float BeginningOfNight; // Значение между 0 и 1, указывающее начало ночи
+    public float TimeOfDay;
+    public float LengthOfDay; // Продолжительность полного дня (включая день и ночь)
     public int Days;
-    public Light LightSun; 
 
     void FixedUpdate()
     {
@@ -18,7 +18,24 @@ public class DayChangerSc : MonoBehaviour
             TimeOfDay = 0;
             Days++;
         }
-                     
-       transform.localRotation = Quaternion.Euler (TimeOfDay *360, 0, 0);       
+
+        // Определяем текущую фазу (день или ночь) и вычисляем угол поворота
+        float currentPhaseTime;
+        if (TimeOfDay < BeginningOfNight)
+        {
+            // Дневная фаза
+            float lengthOfDayPhase = BeginningOfNight * LengthOfDay;
+            currentPhaseTime = TimeOfDay / BeginningOfNight;
+            transform.localRotation = Quaternion.Euler(currentPhaseTime * 180, 0, 0);
+        }
+        else
+        {
+            // Ночная фаза
+            float lengthOfNightPhase = (1 - BeginningOfNight) * LengthOfDay;
+            currentPhaseTime = (TimeOfDay - BeginningOfNight) / (1 - BeginningOfNight);
+            transform.localRotation = Quaternion.Euler(180 + currentPhaseTime * 180, 0, 0);
+        }
+        Debug.Log(currentPhaseTime);
     }
 }
+
