02380c18abfd48d929e7a2109d9dacd2fe7b3276
Ruruner <rustananisev448@gmail.com>
2024-07-26 22:06:12 +0500

    generator FIX

--- Assets/Sc/WeatherSc.cs
+++ Assets/Sc/WeatherSc.cs
@@ -5,31 +5,28 @@ using UnityEngine;
 
 public class WeatherSc : MonoBehaviour
 {
-    public int Wind { get => Wind; private set => Wind = value; }
-    public int Heat { get => Heat; private set => Heat = value; }
-    public int Rain { get => Rain; private set => Rain = value; }
+    [SerializeField] int Wind,Heat,Rain;
+
 
     [SerializeField] float stage_1, stage_2, stage_3;
-    public bool CenerateWeather;
+    public bool GenerateWeather;
    
-    private void Update() { if (CenerateWeather) WeatherChanger(); }
+    private void Update() { if (GenerateWeather) WeatherChanger(); }
 
     public void WeatherChanger() 
     {
-        Wind = CenrateStageWether();
-        Heat = CenrateStageWether();
-        Rain = CenrateStageWether();
-        Debug.LogWarning(Wind + " " + Heat + " " + Rain);
-        CenerateWeather = false;
+        Wind = GenerateStageWeather();
+        Heat = GenerateStageWeather();
+        Rain = GenerateStageWeather();
+        GenerateWeather = false;
     }
-    private int CenrateStageWether() 
+    private int GenerateStageWeather() 
     {
         float value;
         value = Random.Range(0f, 1f);
-        Debug.LogWarning(value);
-             if (value <= stage_1) return 0;
-        else if (value <= stage_2) return 1;
-        else if (value <= stage_3) return 2;
+             if (value < stage_1) return 0;
+        else if (value < stage_2) return 1;
+        else if (value < stage_3) return 2;
         else return 3 ;        
     }
 }
