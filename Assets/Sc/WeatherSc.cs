258077fbfb643d910eebde3c639185479531a9c3
Ruruner <rustananisev448@gmail.com>
2024-07-26 21:47:51 +0500

    base weather

--- Assets/Sc/WeatherSc.cs
+++ Assets/Sc/WeatherSc.cs
@@ -1,22 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
-//using System.Diagnostics;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class WeatherSc : MonoBehaviour
 {
-    public int wind, heat, rain; 
-    public float Stage_1, Stage_2, Stage_3;
+    public int Wind { get => Wind; private set => Wind = value; }
+    public int Heat { get => Heat; private set => Heat = value; }
+    public int Rain { get => Rain; private set => Rain = value; }
+
+    [SerializeField] float stage_1, stage_2, stage_3;
     public bool CenerateWeather;
+   
     private void Update() { if (CenerateWeather) WeatherChanger(); }
 
     public void WeatherChanger() 
     {
-        wind = CenrateStageWether();
-        heat = CenrateStageWether();
-        rain = CenrateStageWether();
-        Debug.LogWarning(wind + " " + heat + " " + rain);
+        Wind = CenrateStageWether();
+        Heat = CenrateStageWether();
+        Rain = CenrateStageWether();
+        Debug.LogWarning(Wind + " " + Heat + " " + Rain);
         CenerateWeather = false;
     }
     private int CenrateStageWether() 
@@ -24,10 +27,9 @@ public class WeatherSc : MonoBehaviour
         float value;
         value = Random.Range(0f, 1f);
         Debug.LogWarning(value);
-             if (value <= Stage_1) return 0;
-        else if (value <= Stage_2) return 1;
-        else if (value <= Stage_3) return 2;
-        else return 3 ;
-        
+             if (value <= stage_1) return 0;
+        else if (value <= stage_2) return 1;
+        else if (value <= stage_3) return 2;
+        else return 3 ;        
     }
 }
